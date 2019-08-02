0ab8e046ae80f2a03991266855abf991a60c8fa7
KC Tan <kctan6@gmail.com>
2019-08-03 07:09:55 +0800

    filling null categories, standardscaler for numerics, allow specification of test size when training single model

--- merlion/merlion.py
+++ merlion/merlion.py
@@ -1,3 +1,4 @@
+import pickle
 import pandas as pd
 import numpy as np
 import lightgbm as lgb
@@ -6,6 +7,8 @@ from sklearn.model_selection import StratifiedKFold, train_test_split, KFold
 from sklearn.metrics import mean_squared_error, roc_auc_score
 from sklearn.preprocessing import LabelEncoder, StandardScaler, OneHotEncoder, OrdinalEncoder
 from sklearn.compose import ColumnTransformer, make_column_transformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
 import shap, logging
 
 logger = logging.getLogger('merlion')
@@ -20,21 +23,6 @@ def generate_train_test(original_df, target_col, test_size=0.2):
     X = df.drop(target_col, axis=1)
     y = df[target_col]
     return train_test_split(X, y, test_size=test_size)
-
-# def generate_encoded_labels(df):
-#     categories = df.select_dtypes(include='object').columns.tolist()
-#     for category in categories:
-#         le = LabelEncoder()
-#         df[category] = le.fit_transform(df[category].astype(str))
-#         df[category] = df[category].astype('category')
-#     return df, categories
-
-# def generate_dummies(df):
-#     for category in df.select_dtypes(include='object').columns:
-#         dummies = pd.get_dummies(df[category], prefix=category)
-#         df = pd.concat([df, dummies], axis=1)
-#         df.drop(category, axis=1, inplace=True)
-#     return df
 
 DEFAULT_PARAMS = {'learning_rate': 0.15, 'metric': 'rmse', 'verbose':-1}
 N_FOLDS = 5
@@ -52,7 +40,10 @@ FEATURE_SPACE = {
     'feature_fraction': (0.7, 0.95)
     }
 
+NO_PICKLE_ITEMS = ['lgb_eval', 'best_params', 'maximize', 'lgbBO', 'lgb_train']
+
 class Merlion:
+    '''Base Class for Machine Learning using LightGBM and Bayes Optimization'''
     def __init__(self, default_params=DEFAULT_PARAMS, feature_space=FEATURE_SPACE, early_stopping_rounds=EARLY_STOPPING_ROUNDS, 
         num_boost_round=NUM_BOOST_ROUND, n_folds=N_FOLDS, random_seed=RANDOM_SEED, metric='rmse'):
         default_params['metric'] = metric
@@ -64,22 +55,32 @@ class Merlion:
         self.random_seed = random_seed
         self.metric = metric
 
-    def fit_transform(self, X):
+    def __getstate__(self):
+        '''Excluding items which cannot be pickled'''
+        return dict((k, v) for (k, v) in self.__dict__.items() if k not in NO_PICKLE_ITEMS)
+
+    def fit_transform(self, X, nan_value_fill=''):
+        '''Fit and transform training dataframe using ordinal encoder for category features'''
         categorical_features = X.select_dtypes(include='object').columns.tolist()
+        num_rows = X.shape[0]
+        categorical_features = [col for col in categorical_features if X[col].nunique() < num_rows]
         numerical_features = X.select_dtypes(exclude='object').columns.tolist()
         logger.info(f'Categorical Features {categorical_features}')
         logger.info(f'Numerical Features {numerical_features}')
         self.preprocess = make_column_transformer(
-            (OrdinalEncoder(), categorical_features), remainder='passthrough')
+            (make_pipeline(SimpleImputer(strategy='constant', fill_value=nan_value_fill), OrdinalEncoder()), categorical_features),
+            (StandardScaler(), numerical_features))
         X_enriched = self.preprocess.fit_transform(X)
         self.feature_names = categorical_features + numerical_features
         self.categorical_features = categorical_features
         return X_enriched
 
     def transform(self, X):
+        '''Transform dataset for testing purposes'''
         return self.preprocess.transform(X)
 
     def maximize(self, X_train, y_train, init_points=1, n_iter=1, acq='poi', xi=1e-4):
+        '''Trigger the optimization function to search the given feature space'''
         self.lgb_train = lgb.Dataset(X_train, y_train, feature_name=self.feature_names, categorical_feature=self.categorical_features, free_raw_data=False)
         self.lgbBO = BayesianOptimization(f=self.lgb_eval, pbounds=self.feature_space)
         self.lgbBO.maximize(init_points=init_points, n_iter=n_iter, acq=acq, xi=xi)
@@ -87,6 +88,7 @@ class Merlion:
         return
 
     def validate_params(self, num_leaves, min_data_in_leaf, colsample_bytree, reg_lambda, max_depth, subsample, feature_fraction):
+        '''Ensure that parameters are acceptable by lightgbm'''
         params = self.default_params.copy()
         params['num_leaves'] = int(round(num_leaves))
         params['min_data_in_leaf'] = int(round(min_data_in_leaf))
@@ -98,6 +100,7 @@ class Merlion:
         return params
 
     def lgb_eval(self, num_leaves, min_data_in_leaf, colsample_bytree, reg_lambda, max_depth, subsample, feature_fraction):
+        '''Optimization function'''
         params = self.validate_params(num_leaves, min_data_in_leaf, 
             colsample_bytree, reg_lambda, max_depth, subsample, feature_fraction)
         cv_result = lgb.cv(params=params, 
@@ -115,9 +118,10 @@ class Merlion:
             metric_value = -metric_value
         return metric_value
 
-    def train_single_model(self, X, y):
+    def train_single_model(self, X, y, test_size=0.2):
+        '''Train a single model based on best parameters identified by optimization search'''
         self.single_model_params = self.validate_params(**self.lgbBO.max['params'])
-        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.2)
+        X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=test_size)
         lgb_train = lgb.Dataset(X_train, y_train, feature_name=self.feature_names, categorical_feature=self.categorical_features, free_raw_data=False)
         lgb_val = lgb.Dataset(X_val, y_val, feature_name=self.feature_names, categorical_feature=self.categorical_features, free_raw_data=False)
         lgbr = lgb.train(self.single_model_params, lgb_train, num_boost_round=self.num_boost_round,
@@ -126,6 +130,7 @@ class Merlion:
         return
 
     def validate_single_model(self, X_test, y_test, metric):
+        '''Validate performance of single model'''
         preds = self.single_model.predict(X_test)
         if metric == 'rmse':
             result = np.sqrt(mean_squared_error(y_test, preds))
@@ -134,6 +139,7 @@ class Merlion:
         return result     
 
     def train_ensemble_models(self, X, y, n=5, n_splits=5):
+        '''Train ensemble models based on top N parameter combinations and using cross validation training'''
         self.ensemble_models_params = self.best_params(self.lgbBO, n)
         logger.info(f'Params of top {n} models:')
         logger.info(self.ensemble_models_params)
@@ -169,6 +175,7 @@ class Merlion:
         return
     
     def best_params(self, lgbBO, n=5):
+        '''Returns best parameters'''
         lgbBO_df = pd.DataFrame([x['params'] for x in lgbBO.res]).join(pd.DataFrame([x['target'] for x in lgbBO.res], columns=['target']))
         top_n = lgbBO_df.sort_values(by='target', ascending=False).head(n).drop('target', axis=1)
         model_params = []
@@ -179,6 +186,7 @@ class Merlion:
         return model_params
 
     def validate_ensemble_models(self, X_test, y_test, metric):
+        '''Validation of ensemble models performance'''
         average_preds = self.generate_ensemble_models_predictions(X_test)
         if metric == 'rmse':
             result = np.sqrt(mean_squared_error(y_test, average_preds))
@@ -187,15 +195,32 @@ class Merlion:
         return result
 
     def generate_shap_values(self, X):
+        '''Generate shap values for single model'''
         self.explainer = shap.TreeExplainer(self.single_model)
         self.shap_values = self.explainer.shap_values(X)
         return self.shap_values
         #shap.summary_plot(self.shap_values, X)
 
     def generate_single_model_predictions(self, X):
+        '''Generate predictions for single model'''
         return self.single_model.predict(X)
 
     def generate_ensemble_models_predictions(self, X):
+        '''Generate predictions for ensemble models'''
         preds = [[x.predict(X) for x in y] for y in self.ensemble_models]
         average_preds = np.mean(np.mean(preds, axis=0), axis=0)
         return average_preds
+
+    def export_single_model(self, filename):
+        '''Export single model as pickle file'''
+        with open(filename, 'wb') as output_file:
+            pickle.dump(self.single_model, output_file)
+    
+    def export_ensemble_models(self, filename):
+        '''Export ensemble models as pickle file'''
+        with open(filename, 'wb') as output_file:
+            pickle.dump(self.ensemble_models, output_file)
+
+    def export_merlion(self, filename):
+        with open(filename, 'wb') as output_file:
+            pickle.dump(self, output_file)
