1b7831b7e8b4464389f191e050df16f1f6b4180e
KC Tan <kctan6@gmail.com>
2019-10-01 11:23:49 +0800

    fixing duplicate keyword arguments in setup.py file

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import find_packages, setup
 setup(name='merlion',
       version='0.1',
       description='Machine Learning Package',
-      url='https://github.com/thufirtan/merlion',
       author='KC Tan',
       author_email='kctan6@gmail.com',
       url = 'https://github.com/thufirtan/merlion',
